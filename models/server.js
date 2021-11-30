19ffc619a2cb6807e359e495bb23c71b73ba4179
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-11-29 22:46:57 -0600

    Separando responsabilidades

--- models/server.js
+++ models/server.js
@@ -1,15 +1,48 @@
 const express = require('express');
+const cors = require('cors');
+
 class Server {
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+
+        //Middlewares
+        this.middlewares();
+        //rutas de la app
         this.routes();
+    }
+    middlewares(){
+        //Cors
+        this.app.use(cors());
+
+        //Directorio publico
+        this.app.use( express.static("public") )
     }
 
     routes(){
-        this.app.get("/", (req, res) =>{
-            res.send("Hola mundo");
+        this.app.get("/api", (req, res) =>{
+            res.status(403).json({
+                msg: "get API",
+            });
+        });
+
+        this.app.put("/api", (req, res) =>{
+            res.status(403).json({
+                msg: "put API",
+            });
+        });
+
+        this.app.delete("/api", (req, res) =>{
+            res.status(403).json({
+                msg: "delete API",
+            });
+        });
+
+        this.app.post("/api", (req, res) =>{
+            res.status(403).json({
+                msg: "post API",
+            });
         });
     }
 
