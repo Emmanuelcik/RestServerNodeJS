7b8dffa24b8b790ece0117614c4e27d65e36432c
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-12-22 16:13:18 -0600

    Añadiendo modelo para rol de usuario y su validación

---
+++ models/rol.js
@@ -0,0 +1,12 @@
+
+const {Schema, model} = require("mongoose");
+
+const RolSchema = Schema({
+    rol: {
+        type: String,
+        required: [true, "rol obligatorio"]
+    }
+});
+
+
+module.exports = model("Rol", RolSchema);
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const {Router} = require("express");
 const { check } = require("express-validator");
 const { usersGet, usersPut, usersDelete, usersPost, usersPatch} = require("../controllers/user");
 const {fieldsValidator} = require("../middlewares/fields-validator");
+const Rol = require("../models/rol");
 
 const router = Router();
 
@@ -10,8 +11,14 @@ router.get("/", usersGet );
 router.post("/",[
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     check("password", "La contraseña es obligatoria").isLength({min: 6}),
-    check("rol", "No es un rol válido").isIn(["Admin_rol", "user_rol"]),
     check("correo", "El correo no es valido").isEmail(),
+    // check("rol", "No es un rol válido").isIn(["Admin_rol", "user_rol"]),
+    check("rol").custom( async (rol= "") =>{
+        const existeRol = await Rol.findOne({rol});
+        if(!existeRol){
+            throw new Error(`El rol ${rol} no existe en la bd`);
+        }
+    }),
     fieldsValidator
 ] ,usersPost);
 
